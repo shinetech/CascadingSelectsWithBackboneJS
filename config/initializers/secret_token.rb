f4a79b8c3831cbd8b67f0441848254a203ff5f9e
Ben Teese <ben.teese@shinetech.com>
2011-07-25 17:08:14 +1000

    Initial Rails server stub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CascadingSelectsWithBackbone::Application.config.secret_token = '3e5481bd3e78373523f07884a7768374b4e37cbf90c76cd0f6b707d1d87a825de4b2f6271cf0f702fa8a4739c3ea56a9784d0306952fa0f093e6a43337ab6bd2'
