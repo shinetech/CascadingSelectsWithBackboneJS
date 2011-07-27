afb1c119a1060de38c59b4b28a0a3d64cc8d57d0
Ben Teese <ben.teese@shinetech.com>
2011-07-27 18:56:07 +1000

    Added multiple location views

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,22 @@
 $(function(){
     var Country = Backbone.Model.extend();
+    var City = Backbone.Model.extend();
+    var Suburb = Backbone.Model.extend();
+    
     var Countries = Backbone.Collection.extend({
         url: 'countries',
         model: Country
     });
-
-    var CountryView = Backbone.View.extend({
+    
+    var Cities = Backbone.Collection.extend({
+        model: City
+    });
+    
+    var Suburbs = Backbone.Collection.extend({
+        model: Suburb
+    });    
+    
+    var LocationView = Backbone.View.extend({
         tagName: "option",
         
         initialize: function(){
@@ -17,20 +28,24 @@ $(function(){
         }
     });
     
-    var CountriesView = Backbone.View.extend({
+    var LocationsView = Backbone.View.extend({
         initialize: function(){
             _.bindAll(this, 'addOne', 'addAll');
             this.collection.bind('reset', this.addAll);
         },        
-        addOne: function(city){
-            $(this.el).append(new CountryView({ model: city }).render().el);
+        addOne: function(location){
+            $(this.el).append(new LocationView({ model: location }).render().el);
         },        
         addAll: function(){
             this.collection.each(this.addOne);
         }
     });
             
-    var countries = new Countries();    
-    new CountriesView({el: $("#country"), collection: countries});    
+    var countries = new Countries();
+    
+    new LocationsView({el: $("#country"), collection: countries});
+    new LocationsView({el: $("#cities"), collection: new Cities()});
+    new LocationsView({el: $("#suburbs"), collection: new Suburbs()});
+    
     countries.fetch();
 });
